24f176bbbe8ff4b365251d77f6d3a310aaaa234f
Yura Iskhakov <romerodeltoro@yandex.ru>
2023-02-03 08:09:19 +0300

    refactor: изменен тип списка хранения истории на Linked

--- src/com/taskmanager/InMemoryHistoryManager.java
+++ src/com/taskmanager/InMemoryHistoryManager.java
@@ -2,18 +2,18 @@ package com.taskmanager;
 
 import com.taskmanager.tasks.Task;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager{
 
-    private List<Task> history = new ArrayList<>();
+    private LinkedList<Task> history = new LinkedList<>();
 
     // Добавление элемента в список просмотров
     @Override
     public void add(Task task) {
         if (history.size() >= 10) {
-            history.remove(0);
+            history.removeFirst();
             history.add(task);
         } else {
             history.add(task);
