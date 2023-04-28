15451e0fb03b4e24d830fc0b182a20e1dd000c28
Yura Iskhakov <romerodeltoro@yandex.ru>
2023-04-28 12:34:49 +0300

    feat: ТЗ8

---
+++ src/com/taskmanager/KVTaskClient.java
@@ -0,0 +1,11 @@
+package com.taskmanager;
+
+public class KVTaskClient {
+
+    public void put(String key, String json) {
+
+    }
+    public String load(String key) {
+        return null;
+    }
+}
